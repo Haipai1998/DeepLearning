0c6e5798ccb354597973e199600cc81ebc039e63
Shuhai Peng <shuhai.peng@intel.com>
2024-04-08 19:37:24 +0800

    load data

--- HW1/test.py
+++ HW1/test.py
@@ -1,21 +1,30 @@
-import torch
 import pandas
+
+import torch
 import torch.utils.data
-
-
-# Pytorch
 from torch.utils.data import random_split
 
 config = {
-    'validation_ratio':0.2,
-    'covid_train_path':'HW1/covid_train.csv'
+    "validation_ratio": 0.2,
+    "covid_train_path": "HW1/covid_train.csv",
+    "seed": 1998,
 }
 
-def GetRealTrainAndValidationData():
-    original_train_data = pandas.read_csv(config['covid_train_path'])
-    print(f'original_train_data len: {len(original_train_data)}')
-    # random_split()
 
-GetRealTrainAndValidationData()
+def GetRealTrainAndValidationData():
+    original_train_data = pandas.read_csv(config["covid_train_path"])
+    tot_data_length = len(original_train_data)
+    train_data_length = int(config["validation_ratio"] * tot_data_length)
+    validation_data_length = tot_data_length - train_data_length
+    train_data, validation_data = random_split(
+        original_train_data,
+        [train_data_length, validation_data_length],
+        generator=torch.Generator().manual_seed(config["seed"]),
+    )
+    return train_data, validation_data
+
+
+train_data, validation_data = GetRealTrainAndValidationData()
+print(train_data)
 # train_data = pd.read_csv('test.csv')
 # print(train_data.values)
