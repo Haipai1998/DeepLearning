52366a4524205d1555eb83ca89524209acba2e21
Shuhai Peng <shuhai.peng@intel.com>
2024-04-10 11:09:19 +0800

    define DataSet/DataLodaer/Model

--- HW1/hack.py
+++ HW1/hack.py
@@ -1,10 +1,24 @@
 import numpy
 
-a = numpy.random.randint(0, 10, size=[4, 5])
-print(a)
+
+class A:
+    def __init__(self) -> None:
+        print("A::init")
 
 
-a1 = a[:, :-1]
-feature_index_list = list(range(a1.shape[1]))
-a2 = a[:, feature_index_list]
-print(f"a1:{a1},\n a2:{a2}")
+class B(A):
+    def __init__(self) -> None:
+        super().__init__()
+        print("B::init")
+
+
+a = A()
+b = B()
+# a = numpy.random.randint(0, 10, size=[4, 5])
+# print(a)
+
+
+# a1 = a[:, :-1]
+# feature_index_list = list(range(a1.shape[1]))
+# a2 = a[:, feature_index_list]
+# print(f"a1:{a1},\n a2:{a2}")
--- HW1/test.py
+++ HW1/test.py
@@ -2,12 +2,46 @@ import pandas
 import numpy
 import torch
 import torch.utils
+import torch.utils.data
+import torch.utils.data.dataloader
 
 config = {
     "validation_ratio": 0.2,
     "covid_train_path": "HW1/covid_train.csv",
     "seed": 1998,
+    "batch_size": 256,
 }
+
+
+class COVID19DataSet(torch.utils.data.Dataset):
+    def __init__(self, feature, res):
+        if res is None:
+            self.res = None
+        else:
+            self.res = res
+        self.feature = feature
+
+    def __getitem__(self, index):
+        if self.res is None:
+            return self.feature[index]
+        else:
+            return self.feature[index], self.res[index]
+
+    def __len__(self):
+        return len(self.feature)
+
+
+class COVIDModel(torch.nn.Module):
+    def __init__(self, input_dimension) -> None:
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(input_dimension, 16),
+            torch.nn.ReLU(),
+            torch.nn.Linear(input_dimension, 8),
+            torch.nn.ReLU(),
+            torch.nn.Linear(input_dimension, 1),
+        )
+    def forward(self,)
 
 
 def SplitTrainAndValidationData():
@@ -38,10 +72,26 @@ def FeatureSelection(train_data, validation_data):
     return train_data[:, :-1], train_res, validation_data[:, :-1], validation_res
 
 
+# Read Data and feature selection
 train_data, validation_data = SplitTrainAndValidationData()
 print(f"train_data:{train_data.shape},validation_data:{validation_data.shape}")
 train_data, train_res, validation_data, validation_res = FeatureSelection(
     train_data, validation_data
 )
 print(f"number of features: {train_data.shape[1]},{validation_data.shape[1]}")
-# torch.utils.datas
+
+# Create COVID19DataSet
+train_dataset = COVID19DataSet(train_data, train_res)
+validation_dataset = COVID19DataSet(validation_data, validation_res)
+print(
+    f"len of train_dataset:{len(train_dataset)},len of validation_dataset:{len(validation_dataset)}"
+)
+
+# Use DataLodaer help load data to main memory with batching
+# epoch: input all data to dl netowrk and finish forward and backward calculation
+train_loader = torch.utils.data.DataLoader(
+    train_data, batch_size=config["batch_size"], shuffle=True, pin_memory=True
+)
+validation_loader = torch.utils.data.DataLoader(
+    validation_dataset, batch_size=config["batch_size"], shuffle=True, pin_memory=True
+)
