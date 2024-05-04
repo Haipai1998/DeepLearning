7566a03b8e4e08f340aec2cd0de51f0b5e7bbf9e
shuhaipeng <764229539@qq.com>
2024-05-04 20:47:09 +0800

    add hw3

---
+++ HW3/train.py
@@ -0,0 +1,14 @@
+import pandas
+import numpy
+import tqdm
+import common
+import math
+import torch
+import torch.utils
+import torch.utils.data
+import torch.utils.data.dataloader
+import argparse
+import csv
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import torch.optim.lr_scheduler as lr_scheduler
